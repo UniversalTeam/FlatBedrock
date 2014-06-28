53333f705b01aabb4a79d72cf32af75c38d54108
big_Xplosion <bigxplosion44@gmail.com>
2014-06-28 15:09:13 +0200

    Initial Commit!

---
+++ src/main/scala/universalteam/flatbedrock/FlatBedrock.scala
@@ -0,0 +1,30 @@
+package universalteam.flatbedrock
+
+import cpw.mods.fml.common.Mod
+import cpw.mods.fml.common.event.{FMLPostInitializationEvent, FMLInitializationEvent, FMLPreInitializationEvent}
+
+@Mod(modid = FlatBedrock.MOD_ID, name = FlatBedrock.MOD_NAME, version = FlatBedrock.MOD_VERSION, modLanguage = "scala", dependencies = "required-after:Forge;after:UniversalCore")
+object FlatBedrock
+{
+    val MOD_ID = "FlatBedrockx"
+    val MOD_NAME = "FlatBedrock"
+    val MOD_VERSION = "@VERSION@"
+
+    @Mod.EventHandler
+    def preInit(event: FMLPreInitializationEvent)
+    {
+
+    }
+
+    @Mod.EventHandler
+    def init(event: FMLInitializationEvent)
+    {
+
+    }
+
+    @Mod.EventHandler
+    def postInit(event: FMLPostInitializationEvent)
+    {
+        
+    }
+}
