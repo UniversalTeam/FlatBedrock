f8a8e00af2bea15c67c813edb813889978f49ced
big_Xplosion <bigxplosion44@gmail.com>
2014-08-06 20:51:42 +0200

    enabled version checking

--- src/main/java/universalteam/flatbedrock/proxies/CommonProxy.java
+++ src/main/java/universalteam/flatbedrock/proxies/CommonProxy.java
@@ -1,12 +1,17 @@
 package universalteam.flatbedrock.proxies;
 
 import cpw.mods.fml.common.registry.GameRegistry;
+import universalteam.flatbedrock.lib.Reference;
 import universalteam.flatbedrock.world.WorldGenFlatBedrock;
+import universalteam.universalcore.version.UCVersion;
+import universalteam.universalcore.version.UCVersionChecker;
 
 public class CommonProxy
 {
 	public void preInit()
 	{
+		UCVersionChecker.registerModVersion(new UCVersion(Reference.MOD_VERSION, "https://raw.githubusercontent.com/UniversalTeam/UCModVersions/master/FlatBedrock/version.json"));
+
 		initWorldGenerators();
 	}
 
