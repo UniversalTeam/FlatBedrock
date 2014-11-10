5288897e9f0d8dc46b312be709cb0f8a0ef04598
big_Xplosion <bigxplosion44@gmail.com>
2014-11-10 17:56:17 +0100

    Fix server startup crash

--- src/main/java/universalteam/flatbedrock/config/Config.java
+++ src/main/java/universalteam/flatbedrock/config/Config.java
@@ -1,10 +1,11 @@
 package universalteam.flatbedrock.config;
 
-import net.minecraft.client.Minecraft;
+import cpw.mods.fml.relauncher.FMLInjectionData;
 
 import java.io.File;
 
 public class Config
 {
-	public static final File configLocation = new File(Minecraft.getMinecraft().mcDataDir, "config" + File.separator + "FlatBedrock");
+	public static final File mcDir = (File) FMLInjectionData.data()[6];
+	public static final File configLocation = new File(mcDir, "config" + File.separator + "FlatBedrock");
 }
